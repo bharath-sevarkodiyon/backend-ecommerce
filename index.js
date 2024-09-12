b881293d4f756ca7dd639b0c0d752e430ce27bd2
bharath-sevarkodiyon <bharathsevarkodiyon2000@gmail.com>
2024-09-12 20:57:07 +0530

    frontend URL

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://frontend-ecommerce-rouge-tau.vercel.app/', // Replace with your frontend URL
+  origin: '*', // Replace with your frontend URL
   credentials: true
 }));
 app.use(cookieParser());
