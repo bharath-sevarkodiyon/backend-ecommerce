7dbcd9cd64d5a60794e875a3cf1263ce594364f8
bharath-sevarkodiyon <Bharath.Sevarkodiyon@gwcdata.ai>
2024-09-13 07:19:11 +0530

    Add files via upload

--- api/viewCartApi.js
+++ api/viewCartApi.js
@@ -5,23 +5,23 @@ const roleValidationMiddleware = require('../middleware/roleValidationMiddleware
 const viewCartApi = express.Router()
 
 viewCartApi.post('/viewcart', 
-    roleValidationMiddleware.isAuthenticated, 
-    roleValidationMiddleware.customerValidation, 
+    // roleValidationMiddleware.isAuthenticated, 
+    // roleValidationMiddleware.customerValidation, 
     viewCartService.createCartService)
 
 viewCartApi.get('/viewcart', 
-    roleValidationMiddleware.isAuthenticated, 
-    roleValidationMiddleware.customerValidation, 
+    // roleValidationMiddleware.isAuthenticated, 
+    // roleValidationMiddleware.customerValidation, 
     viewCartService.getAllCartService)
 
 viewCartApi.put('/viewcart/:id', 
-    roleValidationMiddleware.isAuthenticated, 
-    roleValidationMiddleware.customerValidation, 
+    // roleValidationMiddleware.isAuthenticated, 
+    // roleValidationMiddleware.customerValidation, 
     viewCartService.updateCartService)
 
 viewCartApi.delete('/viewcart/:id', 
-    roleValidationMiddleware.isAuthenticated, 
-    roleValidationMiddleware.customerValidation, 
+    // roleValidationMiddleware.isAuthenticated, 
+    // roleValidationMiddleware.customerValidation, 
     viewCartService.deleteCartService)
 
 // viewCartApi.get('/viewcart/:id', viewCartService.getCartService)
